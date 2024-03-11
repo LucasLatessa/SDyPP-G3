05415f3d068a9273b0eb8c97a0cca5f7785cc4e2
SantiAsenzoChancalay <mateoasenzo02@gmail.com>
2024-03-10 23:06:05 -0300

    script

--- scripts/userdata/script.sh
+++ scripts/userdata/script.sh
@@ -13,8 +13,26 @@ sudo apt install -y mvn
 java --version
 nodejs --version
 
-sudo apt install nginx -y
-sudo systemctl enable nginx
-sudo systemctl start nginx
+#sudo apt install nginx -y
+#sudo systemctl enable nginx
+#sudo systemctl start nginx
 
-sudo systemctl status nginx
+#sudo systemctl status nginx
+
+sudo touch prueba.js
+sudo chmod 777 prueba.js
+
+sudo echo "http = require('node:http');
+listener = function (request, response) {
+   // Send the HTTP header 
+   // HTTP Status: 200 : OK
+   // Content Type: text/html
+   response.writeHead(200, {'Content-Type': 'text/html'});
+
+   // Send the response body as "Hello World"
+   response.end('<h2 style="text-align: center;">Hello World</h2>');
+};
+
+server = http.createServer(listener);
+server.listen(3000);"  >> prueba.js
+sudo node prueba.js
