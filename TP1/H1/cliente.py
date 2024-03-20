7aaf985563612f80bdef89b4a354940450460990
lucaslatessa <lucaslatessa@gmail.com>
2024-03-20 14:58:55 -0300

    Agrega comentarios

--- TP1/H1/cliente.py
+++ TP1/H1/cliente.py
@@ -1,14 +1,17 @@
 import socket
 
+#Host y puerto al que me quiero conectar
 HOST = '35.196.99.208'
 PORT = 8080 
 
 mi_socket = socket.socket()
 
+#Realiza la conexion
 mi_socket.connect((HOST,PORT))
 
+#Enviar mensaje
 mi_socket.send("Hola desde el cliente".encode())
-respuesta = mi_socket.recv(1024) #Buffer
+respuesta = mi_socket.recv(1024).decode() #Buffer
 
 print (respuesta)
 mi_socket.close()
--- TP1/H1/server.py
+++ TP1/H1/server.py
@@ -1,22 +1,22 @@
 import socket
 
-HOST = '0.0.0.0'  # La dirección IP de loopback, localhost
+HOST = '0.0.0.0'  # Cualquier host
 PORT = 8080        # Puerto para escuchar las conexiones entrantes
 
 mi_socket = socket.socket() #Genera socket
 
-mi_socket.bind((HOST,PORT)) #Recibe ip y puerto
+mi_socket.bind((HOST,PORT)) #Establece conexion: Recibe ip y puerto
 
 mi_socket.listen(5) #Cantidad de peticiones en cola
 
 print(f"El servidor está escuchando en {HOST}:{PORT}")
 
 while True:
-    conexion,addr = mi_socket.accept()
+    conexion,direccion = mi_socket.accept() #Acepto las peticiones
     print ("Nueva conexion establecida!")
-    print (addr)
+    print (direccion)
 
-    peticion = conexion.recv(1024)
+    peticion = conexion.recv(1024).decode()
     print (peticion)
 
     conexion.send("Hola, te saludo desde el servidor".encode())
