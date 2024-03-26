5e4fd180b142dd0cc7a255541f81d1f4622e1b1e
lucaslatessa <lucaslatessa@gmail.com>
2024-03-26 00:27:32 -0300

    Resuelto h7

--- TP1/H7/RegistroContactos.py
+++ TP1/H7/RegistroContactos.py
@@ -1,48 +1,54 @@
 import socket
 import json
-
+import threading
+import time
 #Host y puerto del servidor D
-HOSTD = "127.0.0.1"
-PORTD = 65535
+HOST_D = "127.0.0.1"
+PORT_D = 65535
 
-#Lista de contactos
-contactos = []
+# Lista de contactos para la ventana actual
+contactos_actual = []
+# Lista de contactos para la siguiente ventana
+contactos_siguiente = []
 
-#Creacion del servidor
-mi_socket = socket.socket()
-mi_socket.bind((HOSTD,PORTD))
-mi_socket.listen(5)
+def mover_reg():
+    print("Cambio de registros...")
+    global contactos_actual, contactos_siguiente
+    contactos_actual=contactos_siguiente
+    contactos_siguiente=[]
+    print(contactos_actual)
 
-print(f"El servidor está escuchando en {HOSTD}:{PORTD}")
+def servidor():
+    mi_socket = socket.socket()
+    mi_socket.bind((HOST_D, PORT_D))
+    mi_socket.listen(5)
 
+    print(f"El servidor está escuchando en {HOST_D}:{PORT_D}")
+
+    while True:
+        try:
+            #Nueva conexion
+            conexion, addr = mi_socket.accept()
+            print("Nueva conexión con un nodo C establecida!")
+
+            #Recibo el socket del nodo que envio la peticion
+            peticion = json.loads(conexion.recv(1024).decode())
+            print("Petición recibida:", peticion)
+            #Devuelvo la lista de contactos actuales y agrego el nuevo a contactos siguientes
+            respuesta = contactos_actual
+            conexion.send(json.dumps(respuesta).encode())
+            contactos_siguiente.append(peticion)
+
+            conexion.close()
+        except ConnectionAbortedError:
+            print("La conexión fue cerrada por el cliente.")
+        except KeyboardInterrupt:
+            print("Interrupción del servidor. Cerrando...")
+            break
+    mi_socket.close()
+
+servidor_thread = threading.Thread(target=servidor)
+servidor_thread.start()
 while True:
-    try:
-        #Nueva conexion
-        conexion, addr = mi_socket.accept()
-        print("Nueva conexion con un nodo C establecida!")
-
-        #Recibo el socket del nodo que envio la peticion
-        peticion = json.loads(conexion.recv(1024).decode())
-     
-        print("Petición recibida:", peticion)
-
-        #Devuelvo la lista de contacto y agrego al nuevo
-        respuesta = contactos
-        conexion.send(json.dumps(respuesta).encode())
-        contactos.append(peticion)
-
-        #Muestro la lista actualizada
-        print("------------------")
-        print("Lista de contactos actualizada")
-        print("Mis Contactos:")
-        print(contactos)
-        print("------------------")
-
-        conexion.close()
-    except ConnectionAbortedError:
-        print("La conexión fue cerrada por el cliente.")
-    except KeyboardInterrupt:
-        print("Interrupción del servidor. Cerrando...")
-        break
-
-mi_socket.close()
+    time.sleep(60) # Mover registros cada 60 segundos
+    mover_reg()
