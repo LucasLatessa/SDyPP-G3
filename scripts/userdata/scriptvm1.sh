c07c464a0c665737c954f1350846712088d51b99
Josue Gatica Odato <josuegaticaodato@gmail.com>
2024-03-28 14:33:32 -0300

    actualizado scriptvm1

--- scripts/userdata/scriptvm1.sh
+++ scripts/userdata/scriptvm1.sh
@@ -2,16 +2,6 @@
 # Actualizacion de los paquetes
 sudo apt update
 
-# # Instalacion Node.js 18.x
-# sudo apt install -y nodejs
-
-
-# # Instalacion OpenJDK
-# sudo apt install -y openjdk-18-jdk
-# sudo apt install -y mvn
-# java --version
-# nodejs --version
-
 #Instalacion docker
 sudo apt install -y \
     apt-transport-https \
@@ -33,6 +23,8 @@ docker pull lucaslatessa/h2
 docker pull lucaslatessa/h3
 docker pull lucaslatessa/h4
 docker pull lucaslatessa/h5
+docker pull lucaslatessa/h6
+docker pull lucaslatessa/h6contactos
 
 #Correr contenedores
 sudo docker run --name h1 --rm -p 8081:8080 lucaslatessa/h1 > logfileh1.txt 2>&l &
@@ -41,30 +33,16 @@ sudo docker run --name h3 --rm -p 8083:8080 lucaslatessa/h3 > logfileh2.txt 2>&l
 sudo docker run --rm --name h4 -p 8084:8080 lucaslatessa/h4 0.0.0.0 8080 35.185.81.236 8084 > logfileh4.txt 2>&1 &
 sudo docker run --rm --name h5 -p 8085:8080 lucaslatessa/h5 0.0.0.0 8080 35.185.81.236 8085 > logfileh5.txt 2>&1 &
 
- docker pull lucaslatessa/h6
-# docker run --name h6 --rm -p 8086:8080 lucaslatessa/h6 &
+#H6
+#Contactos 
+sudo docker run --rm --name h6contactos -p 8086:8080 lucaslatessa/h6contactos &
+#ServCli.py
+#sudo docker run --rm --name h6 -p $PUERTO_EXT:8080 lucaslatessa/h6 35.196.99.208 8086 &
+
+sudo docker run --rm --name h6 -p $PUERTO_EXT:8080 -e PUERTO_EXT=$PUERTO_EXT lucaslatessa/h6 35.196.99.208 8086
 
  docker pull lucaslatessa/h7
 # docker run --name h7 --rm -p 8087:8080 -e IP_SERVIDOR_DESTINO=35.185.81.236 -e PUERTO_DESTINO=8087 -e IP_CLIENTE=127.0.0.1 -e PUERTO_CLIENTE=8090 lucaslatessa/h6 &
 # docker run --name h7b --rm -p 8090:8080 -e IP_SERVIDOR_DESTINO=35.185.81.236 -e PUERTO_DESTINO=8090 -e IP_CLIENTE=127.0.0.1 -e PUERTO_CLIENTE=8087 lucaslatessa/h6 &
-
-
-# # Instalacion Nginx y poner el marcha el servidor web
-# sudo apt install nginx -y
-# sudo systemctl enable nginx
-# sudo systemctl start nginx
-# sudo systemctl status nginx
-
-# # Creacion de la app node (basica)
-# sudo touch prueba.js
-# sudo chmod 777 prueba.js
-
-# sudo echo "var http = require('http');
-
-# http.createServer(function (req, res) {
-#   res.writeHead(200, {'Content-Type': 'text/html'});
-#   res.end('Hello World!');
-# }).listen(8080);"  >> prueba.js
-# sudo node prueba.js
 
 
