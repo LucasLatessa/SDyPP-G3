e2a7ef7b2f3e8ba6a6b0aac4d50eab8f3a51dde7
Josue Gatica Odato <josuegaticaodato@gmail.com>
2024-03-30 04:50:29 -0300

    H5 funcionando :)

--- TP1/H5/Serv_Cli.py
+++ TP1/H5/Serv_Cli.py
@@ -1,18 +1,58 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
 import socket
 import threading
 import time
 import sys
-import json
 
-HOSTServ = sys.argv[1]  # La dirección IP de loopback, localhost
-PORTServ = int(sys.argv[2])  # Puerto para escuchar las conexiones entrantes
-HOSTDest = sys.argv[3]
-PORTDest = int(sys.argv[4])
+HOSTServ = sys.argv[1]  #n La dirección IP de loopback, localhost
+PORTServ = int(sys.argv[2])        # Puerto para escuchar las conexioes entrantes
+HOSTDest = sys.argv[3] 
+PORTDest = int(sys.argv[4]) 
+PORT_STATUS = 10005 # Puerto status
+
+# ------------------------------------------------------------------------------
+#                                ENDPOINT
+# ------------------------------------------------------------------------------
+
+status = True
+
+#Creamos la ruta
+class StatusHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/status':
+            #Devuelvo un codigo 200 y un mensaje diciendo que el server funciona
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            status_json = json.dumps({
+                "key":"h5",
+                "status": "Servidor en funcionamiento"})
+            self.wfile.write(status_json.encode())
+        else:
+            #Si es otro path dice que en endpoint no existe(404)
+            self.send_response(404)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write("Endpoint no encontrado".encode())
+
+
+def statusRun(server_class=HTTPServer, handler_class=StatusHandler):
+    #Crea el socket, y deja corriendo /status
+    server_address = (HOSTServ, PORT_STATUS)
+    httpd = server_class(server_address, handler_class)
+    print("Status corriendo!")
+    while status:
+        httpd.serve_forever() #Maneja solicitudes
+
+# ------------------------------------------------------------------------------
+#                                APLICACION
+# ------------------------------------------------------------------------------
 
 def servidor():
-    mi_socket = socket.socket()  # Genera socket
-    mi_socket.bind((HOSTServ, PORTServ))  # Recibe ip y puerto
-    mi_socket.listen(5)  # Cantidad de peticiones en cola
+    mi_socket = socket.socket() # Genera socket
+    mi_socket.bind((HOSTServ, PORTServ)) # Recibe ip y puerto
+    mi_socket.listen(5) # Cantidad de peticiones en cola
 
     print(f"El servidor está escuchando en {HOSTServ}:{PORTServ}")
 
@@ -27,17 +67,16 @@ def servidor():
 
             respuesta = {"mensaje": "Hola, te saludo desde el servidor"}
             conexion.send(json.dumps(respuesta).encode())  # Envia respuesta como JSON
-
+            
         except ConnectionAbortedError:
             print("La conexión fue cerrada por el cliente.")
         except KeyboardInterrupt:
             print("Interrupción del servidor. Cerrando...")
         except BrokenPipeError:
-            print("Se produjo un error de pipe roto.")
+                print("Se produjo un error de pipe roto.")
         finally:
             conexion.close()
 
-    mi_socket.close()
 
 def conectar():
     mi_socket = socket.socket()
@@ -47,7 +86,7 @@ def conectar():
             return mi_socket
         except ConnectionRefusedError:
             print("La conexión fue rechazada. Intentando de nuevo...")
-            time.sleep(5)  # Esperar 5 segundos antes de intentar de nuevo
+            time.sleep(5)  # Esperar 5 segundos antes de intentar de nuevo      
             continue
         except KeyboardInterrupt:
             raise  # Volver a lanzar la excepción para que el bloque principal pueda manejarla
@@ -59,7 +98,7 @@ def enviar_saludo(socket_cliente):
             socket_cliente.send(json.dumps(mensaje).encode())  # Envia mensaje como JSON
             respuesta = json.loads(socket_cliente.recv(1024).decode())  # Decodifica respuesta JSON
             print("Respuesta recibida:", respuesta)
-        except (ConnectionResetError, ConnectionAbortedError,BrokenPipeError):
+        except (ConnectionResetError, ConnectionAbortedError, BrokenPipeError):
             print("Se produjo un error de conexión. Reintentando (Cliente)...")
             socket_cliente.close()
             socket_cliente = conectar()  # Usamos una nueva variable para el nuevo socket
@@ -71,7 +110,7 @@ def enviar_saludo(socket_cliente):
             pass
         time.sleep(10)  # Esperar 10 segundos antes de enviar el próximo saludo
 
-def cliente():
+def cliente ():
     try:
         mi_socket = conectar()
         enviar_saludo(mi_socket)
@@ -79,11 +118,16 @@ def cliente():
     except KeyboardInterrupt:
         print("Interrupción del cliente. Cerrando...")
 
+
 if __name__ == "__main__":
     if len(sys.argv) != 5:
         print("Uso: python serv_cli.py <ip_servidor> <puerto_servidor> <ip_destino> <puerto_destino>")
         sys.exit(1)
 
+    #Ejecutar servidor en otro hilo
     servidor_thread = threading.Thread(target=servidor)
     servidor_thread.start()
+    # Ejecutar el servidor HTTP en un hilo separado
+    threading.Thread(target=statusRun, daemon=True).start()
+    #servidor()
     cliente()
