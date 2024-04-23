d0228c5a85497f45b3afd512cffc317c14ab72ec
lucaslatessa <lucaslatessa@gmail.com>
2024-04-23 11:41:28 -0300

    H1 primer subida

---
+++ TP3/H1/main.tf
@@ -0,0 +1,26 @@
+
+# Configuración de Google Cloud Platform
+provider "google" {
+  credentials = file("credentials.json")
+  project     = "organic-premise-416700"
+  region      = "us-east1"
+}
+
+# Recurso de instancia de Google Compute Engine
+resource "google_compute_instance" "ejemplo" {
+  count        = var.numero_instancias
+  name         = "instancia-${count.index + 1}"
+  machine_type = "n1-standard-1"
+  zone         = " us-east1-b"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-10"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+}
