9e235c3bf08a6fcd6802df2848482a33f20ff5dd
lucaslatessa <lucaslatessa@gmail.com>
2024-04-24 16:26:54 -0300

    H2 terminado

---
+++ TP3/H2/paquetes.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo apt update -y 
+sudo apt install -y nginx
+sudo apt-get install -y wget htop
---
+++ TP3/H2/variables.tf
@@ -0,0 +1,11 @@
+
+variable "numero_instancias" {
+    type=number
+    default= 3
+    
+    }
+variable "metadata_startup_script" {
+    type= string
+    default= "paquetes.sh"
+
+}
