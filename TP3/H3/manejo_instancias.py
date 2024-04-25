d569ac3568e269ff3ad84b172ffb8916f64ac077
Josue Gatica Odato <josuegaticaodato@gmail.com>
2024-04-24 23:24:59 -0300

    Problema de ip solucionado

--- TP3/H3/manejo_instancias.py
+++ TP3/H3/manejo_instancias.py
@@ -1,6 +1,13 @@
-#Crear instancia
-def crear_instancia(nombre,zone, project_id, instancia_cliente):
+from google.cloud import compute_v1
+
+
+# Crear instancia
+def crear_instancia(nombre, zone, project_id, instancia_cliente):
     # Definir la configuración de la instancia
+    access = compute_v1.AccessConfig()
+    access.type_ = compute_v1.AccessConfig.Type.ONE_TO_ONE_NAT.name
+    access.name = "External NAT"
+
     instance_body = {
         "name": f"{nombre}",
         "machine_type": f"zones/{zone}/machineTypes/n1-standard-1",
@@ -10,51 +17,62 @@ def crear_instancia(nombre,zone, project_id, instancia_cliente):
                 "auto_delete": True,
                 "initialize_params": {
                     "source_image": "projects/debian-cloud/global/images/family/debian-10"
-                }
+                },
             }
         ],
         "network_interfaces": [
             {
                 "network": "global/networks/default",
-                "access_configs": [{}]
+                "access_configs": [{
+                }]
             }
-        ]
+        ],
     }
 
     # Crear la instancia
-    operation = instancia_cliente.insert(project=project_id, zone=zone, instance_resource=instance_body,)
+    operation = instancia_cliente.insert(
+        project=project_id,
+        zone=zone,
+        instance_resource=instance_body,
+    )
 
     print("Creando la instancia...")
     operation.result()
     print("Instancia creada exitosamente.")
 
-#Listar instancias
+
+# Listar instancias
 def listar_instancias(zone, project_id, instancia_cliente):
-    #Obtengo la lista de instancias
+    # Obtengo la lista de instancias
     lista_instancias = instancia_cliente.list(project=project_id, zone=zone)
 
     print("Instancias:")
     count = 1
     for instancia in lista_instancias:
-        print(f" {count} | ID: {instancia.id} | Nombre: {instancia.name} | Estado: {instancia.status}")
+        print(
+            f" {count} | ID: {instancia.id} | Nombre: {instancia.name} | Estado: {instancia.status}"
+        )
         count += 1
 
-#Pausar instancia
-def pausar_instancia(nombre,zone, project_id, instancia_cliente):
+
+# Pausar instancia
+def pausar_instancia(nombre, zone, project_id, instancia_cliente):
     # Detener la instancia
     instancia_cliente.stop(project=project_id, zone=zone, instance=nombre)
 
     print(f"Instancia '{nombre}' pausada correctamente.")
 
-#Reiniciar instancia
-def reiniciar_instancia(nombre,zone, project_id, instancia_cliente):
+
+# Reiniciar instancia
+def reiniciar_instancia(nombre, zone, project_id, instancia_cliente):
     # Reiniciar la instancia
     instancia_cliente.reset(project=project_id, zone=zone, instance=nombre)
 
     print(f"Instancia '{nombre}' reiniciada correctamente.")
 
-#Eliminar instancia
-def eliminar_instancia(nombre,zone, project_id, instancia_cliente):
+
+# Eliminar instancia
+def eliminar_instancia(nombre, zone, project_id, instancia_cliente):
     # Eliminar la instancia
     instancia_cliente.delete(project=project_id, zone=zone, instance=nombre)
 
