4418815d3f060f6df2d24b7b426098547fd6ebe2
Josue Gatica Odato <josuegaticaodato@gmail.com>
2024-05-16 23:58:00 -0300

    Asi como esta anda el H2

--- TP4/H2/paquetes.sh
+++ TP4/H2/paquetes.sh
@@ -15,12 +15,34 @@ echo \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
 sudo apt install -y docker-ce docker-ce-cli containerd.io
-#docker --version
+
+# Agregar el usuario actual al grupo docker
 sudo usermod -aG docker $USER
 
+# Recargar los permisos del grupo docker
+newgrp docker
+
+# Esperar un poco para asegurarse de que Docker esté completamente cargado
+sleep 10
+
 # Clonar los contenedores
-docker pull josuegaticaodato/servidor_sobel
+sudo docker pull josuegaticaodato/servidor_sobel
+
+# Verificar si el pull fue exitoso
+if [ $? -ne 0 ]; then
+    echo "Error: El pull de la imagen Docker falló."
+    exit 1
+fi
+
+# Esperar un poco más antes de ejecutar el contenedor
+sleep 30
 
 # Correr el contenedor
-sudo docker run -it --rm --name servidor_sobel -p 5000:5000 josuegaticaodato/servidor_sobel
+sudo docker run --rm --name servidor_sobel -p 5000:5000 josuegaticaodato/servidor_sobel
+
+# Verificar si el contenedor se ejecutó correctamente
+if [ $? -ne 0 ]; then
+    echo "Error: El contenedor Docker no se ejecutó correctamente."
+    exit 1
+fi
 
