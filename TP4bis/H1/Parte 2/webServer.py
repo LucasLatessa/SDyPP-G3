e9e932cee737c87992654027b8f17e1a88b84541
Josue Gatica Odato <josuegaticaodato@gmail.com>
2024-05-29 19:43:32 -0300

    Funcionando parte 2 sin DC

--- TP4bis/H1/Parte 2/webServer.py
+++ TP4bis/H1/Parte 2/webServer.py
@@ -52,37 +52,6 @@ def imagen_a_base64(imagen):
     imagen_base64 = base64.b64encode(buffer).decode('utf-8')
     return imagen_base64
 
-# Encargado de particionar las imagenes en x e y
-def particionar_imagen(image, num_particiones_x, num_particiones_y):
-    # Obtén las dimensiones de la imagen
-    height, width, _ = image.shape
-
-    # Calcula el tamaño de cada partición
-    part_height = height // num_particiones_y
-    part_width = width // num_particiones_x
-
-    # Lista para almacenar las particiones
-    particiones = []
-
-    # Particiona la imagen
-    for i in range(num_particiones_y):
-        for j in range(num_particiones_x):
-            # Calcula los límites de cada partición
-            y_start = i * part_height
-            y_end = y_start + part_height
-            x_start = j * part_width
-            x_end = x_start + part_width
-
-            # Extrae la partición
-            particion = image[y_start:y_end, x_start:x_end]
-            particiones.append(particion)
-
-            # Solo para mostrar como quedan las particiones
-            # cv2.imwrite(f'particion{i}_{j}.jpg', particion)
-
-    # print("Imagen particionada!")
-    return particiones
-
 # Encargado de unir todas las imagenes
 def unir_particiones(particiones_sobel):
     # Calculo el tamaño (número de particiones en x y en y)
---
+++ TP4final/H1/Parte 2/Workers/worker.py
@@ -0,0 +1,79 @@
+import cv2  # Biblioteca para el procesamiento de imagenes
+import numpy as np  # Biblioteca para la computacion cientifica en Python, que permite realizar operaciones matematicas eficientes en matrices y matrices mutldimiensionales.
+import pika
+import redis
+
+# Funcion que aplica sobel
+def sobel(imagen):
+    # print(imagen)
+    # Primero convierto la imagen a escala de grises
+    gris = cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)
+
+    # Aplico el operador de Sobel en los ejes X e Y
+    sobel_x = cv2.Sobel(gris, cv2.CV_64F, 1, 0, ksize=3)  # Detecta bordes verticales
+    sobel_y = cv2.Sobel(gris, cv2.CV_64F, 0, 1, ksize=3)  # Detecta bordes horizontales
+
+    # Calculo la magnitud del gradiente, combinando los resultados de X e Y
+    magnitud = np.sqrt(sobel_x**2 + sobel_y**2)
+
+    # Normaliza la magnitud, asi la convierto ea imagen de 8 bits
+    magnitud = cv2.normalize(magnitud, None, 0, 255, cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+    # De esta forma, los valores estan en el rango de 0 a 255
+
+    # Retorno la imagen con el filtro (bordes resaltados)
+    return magnitud
+
+# Configuracion Reddis
+host_redis = "localhost"
+r = redis.Redis(host=host_redis, port=6379, decode_responses=False)
+
+# Me conecto con rabbit
+host_rabit = "localhost"
+nombre_queue = "image_parts"
+connection = pika.BlockingConnection(pika.ConnectionParameters(host=host_rabit))
+channel = connection.channel()
+
+# Creo la cola en el caso de que no exista
+channel.queue_declare(queue=nombre_queue, durable=True)
+print(" Esperando mensajes. Toque CTRL+C para salir")
+
+
+# Funcion que se ejecuta cada vez que recibo un mensaje
+def callback(ch, method, properties, body):
+
+    # Decodifico el identificador y el nombre de la imagen
+    id = properties.headers.get("id")
+    parte = properties.headers.get("parte")
+    nombre_imagen = properties.headers.get("nombre")
+
+    print(f" Imagen recibida! - ID:{id} - Parte: {parte}")
+
+    # La convierto a nparray para poder aplicarle Sobel
+    imagen_bytes = np.frombuffer(body, np.uint8)
+    imagen_np = cv2.imdecode(imagen_bytes, cv2.IMREAD_COLOR)
+    imagen_sobel = sobel(imagen_np)
+
+    print(" Filtro sobel aplicado! ")
+
+    # Codifico la imagen a bytes
+    _, buffer = cv2.imencode(".jpg", imagen_sobel)
+    imagen_redis = buffer.tobytes()
+
+    # Envío la imagen procesada a Redis
+    redis_key = f"{id}_{parte}"
+    r.set(redis_key, imagen_redis)
+
+    print(f"Imagen procesada y almacenada en Redis. Clave: {redis_key}")
+
+    ch.basic_ack(
+        delivery_tag=method.delivery_tag
+    )  # Tengo ACK, de esta forma si se da de baja un workers no pierdo los mensajes.
+
+
+channel.basic_qos(
+    prefetch_count=1
+)  # Elimina el problema de RoundRobin y los mensajes sin trabajar mientras espera un worker
+channel.basic_consume(queue=nombre_queue, on_message_callback=callback)
+
+# Dejo el consumidor en escucha
+channel.start_consuming()
