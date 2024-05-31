3f6a9d5a4388b895b56dee18991e842f2da813e1
Josue Gatica Odato <josuegaticaodato@gmail.com>
2024-05-31 18:26:44 -0300

    (go) cambio en apis.tf

--- TP4final/H3/Terraform/02-apis.tf
+++ TP4final/H3/Terraform/02-apis.tf
@@ -1,10 +1,14 @@
 # # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/google_project_service
 resource "google_project_service" "compute" {
   service = "compute.googleapis.com"
+
+  disable_dependent_services = true
 }
 
 resource "google_project_service" "container" {
   service = "container.googleapis.com"
+
+  disable_dependent_services = true
 }
 
 resource "google_project_service" "cloud_resource_manager" {
