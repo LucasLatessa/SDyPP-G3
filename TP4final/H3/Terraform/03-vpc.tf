b088c568ef226b9f3f8f24221ae846dd592ded70
Josue Gatica Odato <josuegaticaodato@gmail.com>
2024-05-31 18:07:09 -0300

    (go) cambio valor en vpc.if

--- TP4final/H3/Terraform/03-vpc.tf
+++ TP4final/H3/Terraform/03-vpc.tf
@@ -1,9 +1,10 @@
 
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_network
 resource "google_compute_network" "main" {
+  count                           = length(data.google_compute_network.existing.*.self_link) == 0 ? 1 : 0
   name                            = "main"
   routing_mode                    = "REGIONAL"
-  auto_create_subnetworks         = false
+  auto_create_subnetworks         = true
   mtu                             = 1460
   delete_default_routes_on_create = false
 
