d4a460c42ef54dba5a1e0ba6a6b3d39ff6602670
Josue Gatica Odato <josuegaticaodato@gmail.com>
2024-05-31 23:35:29 -0300

    (go) Probando accion, borre todo lo que quedo dando vueltas

--- TP4final/H3/Terraform/03-vpc.tf
+++ TP4final/H3/Terraform/03-vpc.tf
@@ -1,5 +1,4 @@
-
-# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_network
+# # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_network
 resource "google_compute_network" "main" {
   name                            = "main"
   routing_mode                    = "REGIONAL"
--- TP4final/H3/Terraform/10-global-ip.tf
+++ TP4final/H3/Terraform/10-global-ip.tf
@@ -1,5 +1,5 @@
 #gcloud compute addresses create traefik-ip --global
 
-resource "google_compute_global_address" "traefik-ip" {
-  name = "traefik-ip"
-}
+# resource "google_compute_global_address" "traefik-ip" {
+#   name = "traefik-ip"
+# }
