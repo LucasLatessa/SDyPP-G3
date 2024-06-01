a28e1174e25b11a4f55a2a63142b35c2ba4ab6cc
Josue Gatica Odato <josuegaticaodato@gmail.com>
2024-05-31 22:32:39 -0300

    (go) Test accion para ver si funciona y levanta todo

--- TP4final/H3/Terraform/02-apis.tf
+++ TP4final/H3/Terraform/02-apis.tf
@@ -1,14 +1,10 @@
 # # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/google_project_service
 resource "google_project_service" "compute" {
   service = "compute.googleapis.com"
-
-  disable_dependent_services = true
 }
 
 resource "google_project_service" "container" {
   service = "container.googleapis.com"
-
-  disable_dependent_services = true
 }
 
 resource "google_project_service" "cloud_resource_manager" {
