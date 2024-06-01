1130629bdacc9594649584109ed62ac2ab0ef85a
Josue Gatica Odato <josuegaticaodato@gmail.com>
2024-06-01 02:43:52 -0300

    Eliminado TP4/bis

--- TP4bis/H2/Terraform/rabbit.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo apt-get update
-sudo apt-get install -y rabbitmq-server
-sudo systemctl enable rabbitmq-server
-sudo systemctl start rabbitmq-server
--- TP4bis/H2/Terraform/redis.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo apt-get update
-sudo apt-get install -y redis-server
-sudo systemctl enable redis-server
-sudo systemctl start redis-server
