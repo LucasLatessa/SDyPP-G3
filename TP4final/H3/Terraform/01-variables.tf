69d7dc14aa21b5586fbe2ced22f729fd21679d3f
lucaslatessa <lucaslatessa@gmail.com>
2026-09-13 14:50:50 -0300

    no credentials_json plano (TP4)

--- TP4final/H3/Terraform/01-variables.tf
+++ TP4final/H3/Terraform/01-variables.tf
@@ -10,7 +10,7 @@ variable "startup_worker" {
 
 variable "nombre_proyecto" {
   type    = string
-  default = "sdypp-2024"
+  default = "project-c0ccded1-22c1-4a9e-85f"
 }
 
 variable "tipo_maquina" {
